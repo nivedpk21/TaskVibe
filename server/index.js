a994ab12d5e19cd71dec4526532d7553339e45e2
Nived PK <162270753+nivedpk21@users.noreply.github.com>
2025-01-13 15:07:54 +0530

    updated mongodb connection code

--- server/index.js
+++ server/index.js
@@ -25,11 +25,7 @@ if (!process.env.MONGODB_URI) {
 }
 
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("connected to mongoDB"))
   .catch((error) => {
     console.error("mongoDb connection error", error);
