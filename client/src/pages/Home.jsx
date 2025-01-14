253fd5f973b613795acd5aa79f3024a7ec0aeecb
Nived PK <162270753+nivedpk21@users.noreply.github.com>
2025-01-14 19:54:45 +0530

    design changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,8 +9,6 @@ import Footer from "../layout/Footer";
 injectSpeedInsights();
 
 /*chat gpt scanned and optimised*/
-/**testing deployment railway */
-//client changes
 
 export default function Home() {
   return (
@@ -22,13 +20,13 @@ export default function Home() {
           <div className="landing-section">
             <div className="first-section-div ">
               <h1 className="landing-text">
-                Digital Marketing <br /> And Earnings <br />
-                Platform
+                Smart Solutions <br />
+                for Digital <br /> Success
               </h1>
               <p className="landing-sub-text">
-                Receive instant traffic for your affiliate links and <br /> earn money by completing micro
-                tasks
+                Boost Your Campaigns, Amplify Your Reach, <br /> and Grow Your Online Presence Instantly!
               </p>
+              <p className="landing-start-text">Get Started !</p>
               <div className="landing-btn-div mb-3">
                 <Link type="button" className="btn btn-lg btn-primary rounded-5 me-4" to="/signup">
                   Sign up
@@ -45,50 +43,16 @@ export default function Home() {
           <div className="second-section">
             <h1 className="text-center">Features</h1>
             <div className="second-section-div  d-flex">
-              <div className="features text-center border rounded-3 border-light bg-light">
-                <h3>Micro Tasks</h3>
-                <p>
-                  Love games ? Sounds great! Win big in our <br /> games: Dice, Crash, Limbo, Plinko and
-                  <br />
-                  Roulette
+              <div className="feature text-center border rounded-3">
+                <h3>Promote Content</h3>
+                <p className="feature-text">
+                  Create and share shortUrl, direct link, microtask to drive targeted traffic
                 </p>
               </div>
-              <div className="features text-center border rounded-3 border-light bg-light">
-                <h3 className="">Payed To Click</h3>
-                <p>
-                  Love games ? Sounds great! Win big in our <br /> games: Dice, Crash, Limbo, Plinko and
-                  <br />
-                  Roulette
-                </p>
-              </div>
-              <div className="features text-center   border rounded-3 border-light bg-light">
-                <h3>ShortUrl</h3>
-                <p>
-                  Love games ? Sounds great! Win big in our <br /> games: Dice, Crash, Limbo, Plinko and
-                </p>
-              </div>
-              <div className="features text-center   border rounded-3 border-light bg-light">
-                <h3>Surveys</h3>
-                <p>
-                  Love games ? Sounds great! Win big in our <br /> games: Dice, Crash, Limbo, Plinko and
-                  <br />
-                  Roulette
-                </p>
-              </div>
-              <div className="features text-center   border rounded-3 border-light bg-light">
-                <h3>Games</h3>
-                <p>
-                  Love games ? Sounds great! Win big in our <br /> games: Dice, Crash, Limbo, Plinko and
-                  <br />
-                  Roulette
-                </p>
-              </div>
-              <div className="features text-center  border rounded-3 border-light bg-light">
-                <h3>Advertising</h3>
-                <p>
-                  Love games ? Sounds great! Win big in our <br /> games: Dice, Crash, Limbo, Plinko and
-                  <br />
-                  Roulette
+              <div className="feature text-center border rounded-3">
+                <h3>Earn Credit</h3>
+                <p className="feature-text">
+                  Earn credits by clicking shortUrl, direct link, completing microtask and More...
                 </p>
               </div>
             </div>
