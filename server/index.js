52b0c783770a9eeeefe5e99d67ba3ea15752a188
Nived PK <162270753+nivedpk21@users.noreply.github.com>
2025-01-14 13:42:28 +0530

    test two server changes

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ const userRouter = require("./src/routes/userRouter");
 const adminRouter = require("./src/routes/adminRouter");
 
 // testing vercel if server changes
+// test tow server change
 // setting up app
 const app = express();
 const PORT = 4000; 
