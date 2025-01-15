a4ebbc917f12d10d2522733fb2e690876decb969
Nived PK <162270753+nivedpk21@users.noreply.github.com>
2025-01-15 11:56:52 +0530

    client changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,7 +9,7 @@ import Footer from "../layout/Footer";
 injectSpeedInsights();
 
 /*chat gpt scanned and optimised*/
-/*server url changed*/
+/*client url changed*/
 
 export default function Home() {
   return (
